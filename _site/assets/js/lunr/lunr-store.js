ffebf9441e70dd86f6c21353d8ad3aabd37bcd2e
hbvj99 <helloblazevijay99@gmail.com>
2020-03-27 11:12:09 +0545

    update theme, image

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,7 +2,7 @@ var store = [{
         "title": "Air Pollution Visualization using Open Data",
         "excerpt":"This is a small project I did when I was in college, the use of open data to create effective air pollution awareness among the community using JQuery. View how the air quality is changing, health effect it offers using chart visualization provided by CanvasJS, save the visualization in image...","categories": ["Guides"],
         "tags": ["JavaScript","JQuery","Visualizaiton","Emission","Air Pollution"],
-        "url": "http://localhost:4000/2018/12/air-pollution-visualization-using-open-data.html",
+        "url": "http://localhost:4000/2019/01/air-pollution-visualization-using-open-data.html",
         "teaser":null},{
         "title": "NetTv Box Easy Hack",
         "excerpt":"NetTv IPTV box easy hack This is a simple guide to install stock(ish) android tv firmware to NetTv devices. You will be able to use the android platform into your televison after sucessfully flashing the proper firmware. NetTv interface is basically a custom or limited software that runs on the...","categories": ["Hacks"],
@@ -13,4 +13,9 @@ var store = [{
         "excerpt":"Whitespaces and Liquid template Whitespaces are common blank spaces that you would see in the HTML document when rendered, it can be horizontal or vertical. You may not have noticed whitespaces when using Jekyll unless you view source, it by default uses Liquid templating language to process document like this...","categories": ["Guides"],
         "tags": ["Jekyll","Liquid"],
         "url": "http://localhost:4000/2019/11/liquid-whitespaces-in-jekyll.html",
+        "teaser":null},{
+        "title": "Internet Explorer detection using javascript",
+        "excerpt":"Internet Explorer Back in the early days when using Dail-up or ADSL connections, Internet Explorer browser was well liked browser offered by Microsoft to the Windows line of operating systems which were started in 1995. It is still present in most of the Windows Operating System like Windows 10 and...","categories": ["Guides"],
+        "tags": ["JavaScript","Browser"],
+        "url": "http://localhost:4000/2020/02/internet-explorer-detection-using-javascript.html",
         "teaser":null}]
